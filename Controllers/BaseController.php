e9c6c1f9497a403a48a6580723ad668d9027ee37
hacklego <igsalgado@esei.uvigo.es>
2017-01-02 23:48:45 +0100

    Quick fix

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -11,7 +11,7 @@ class BaseController {
   
     public function __construct() {
         $this->view = ViewManager::getInstance();
-        $this->Permissions = new Permissions();
+        $this->permissions = new Permissions();
    
         if (session_status() == PHP_SESSION_NONE) {      
             session_start();
