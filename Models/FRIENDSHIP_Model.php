2b8b19d4c3cc1d917f369b54c994440251b12d0e
hacklego <igsalgado@esei.uvigo.es>
2017-01-05 01:23:02 +0100

    Added Friendship entity and model. TODO: Controller

---
+++ Models/FRIENDSHIP_Model.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once(__DIR__."/../core/PDOConnection.php");
+
+class USER_Model {
+       
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = PDOConnection::getInstance();
+    }
+
+    public function showall($currentuserid)
+    {
+        $friends = array();
+        
+        $sql = $this->db->prepare("SELECT u.id, u.user, u.name, u.surname FROM user as u, friendship as f where f.member=? AND f.status=1 ORDER BY u.name");
+        $sql->execute(array($currentuserid));
+        $friends_db = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($friends_db as $friend) {
+            array_push($friends, new User($user["id"], $user["user"], $user["name"], $user["surname"]));
+        }
+
+        $sql = $this->db->prepare("SELECT u.id, u.user, u.name, u.surname FROM user as u, friendship as f where f.secondarymember=? AND f.status=1 ORDER BY u.name");
+        $sql->execute(array($currentuserid));
+        $friends_db = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($friends_db as $friend) {
+            array_push($friends, new User($user["id"], $user["user"], $user["name"], $user["surname"]));
+        }
+
+        return $friends;
+    }
+
+    public function showcurrent($friendshipID)
+    {
+        $sql = $this->db->prepare("SELECT * FROM friendship WHERE id=?");
+        $sql->execute(array($friendshipID));
+        $friendship = $sql->fetch(PDO::FETCH_ASSOC);
+
+        if($friendship != NULL) {
+            return new Friendship($friendship["id"], $friendship["member"], $friendship["secondarymember"], $friendship["status"]);
+        } else {
+            return NULL;
+        }
+    }
+
+    public function add(Friendship $friendship) {
+        $sql = $this->db->prepare("INSERT INTO friendship(member,secondarymember,status) values (?,?,?)");
+        $sql->execute(array($friendship->getMember(), $friendship->getSecondaryMember(), $friendship->getStatus()));
+    }
+
+    public function edit(Friendship $friendship)
+    {
+        $sql = $this->db->prepare("UPDATE friendship SET member=?, secondarymember=?, status=? where id=?");
+        $sql->execute(array($friendship->getMember(), $friendship->getSecondaryMember(), $friendship->getStatus()));
+    }
+
+    public function delete(Friendship $friendship)
+    {
+        $sql = $this->db->prepare("DELETE FROM friendship where id=?");
+        $sql->execute(array($friendship->getID()));
+    }
+
+    public function friendExists($member, $secondarymember)
+    {
+        $sql = $this->db->prepare("SELECT count(id) FROM friendship where member=? AND secondarymember=?");
+        $sql->execute(array($member, $secondarymember));
+
+        if ($sql->fetchColumn() > 0) {
+            return true;
+        }
+    }    
+}
---
+++ Models/Friendship.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once(__DIR__."/../core/ValidationException.php");
+
+class User {
+
+    private $id;
+    private $member;
+    private $secondarymember;
+    private $status;
+
+
+
+    public function __construct($id=NULL, $member=NULL, $secondarymember=NULL, $status=0)
+    {
+     $this->id = $id;
+     $this->member = $member;
+     $this->secondarymember = $secondarymember;
+     $this->status = $status;
+    }
+
+    public function getID() {
+        return $this->id;
+    }
+
+    public function setID($id) {
+        $this->id = $id;
+    }
+
+    public function getMember() {
+        return $this->member;
+    }
+
+    public function setMember($member) {
+        $this->member = $member;
+    }
+
+    public function getSecondaryMember() {
+        return $this->secondarymember;
+    }
+
+    public function setSecondaryMember($secondarymember) {
+        $this->secondarymember = $secondarymember;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
+}
