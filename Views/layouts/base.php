68bf847624a7a90b3b953ca9105c95847af538cc
Adrián Simón <adriansimn15@gmail.com>
2017-01-06 14:29:18 +0100

    user profile ready to debug

--- Views/layouts/base.php
+++ Views/layouts/base.php
@@ -42,7 +42,7 @@ $userid = $view->getVariable("currentuserid");
   <div class="container-fluid col-md-10 col-md-offset-1">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false">
         <span class="sr-only"><?= i18n("Toggle navigation") ?></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -52,7 +52,7 @@ $userid = $view->getVariable("currentuserid");
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="collapse-1">
       <ul class="nav navbar-nav">
         <ul class="nav navbar-nav navbar-left">
         <li class="dropdown">
--- Views/user/USER_SHOWCURRENT_Vista.php
+++ Views/user/USER_SHOWCURRENT_Vista.php
@@ -3,54 +3,96 @@
 $view = ViewManager::getInstance();
 	/*
 		index.php?controller=user&action=view&user=($user)
-		View: User profile
-		
-		Input: 
-				- Consult User -> $user
-					-tipo perfil
-
 	*/
 
 	$user = $view->getVariable("user");
+	$friends = $view->getVariable("friends");//friendship
+	$publications = $view->getVariable("publications");
+	$documents = $view->getVariable("documents");
  ?>
 
-	
-
- 	<!-- se user = currentuser mostrar edit perfil -->
-
-	<div class="col-md-2">
-		<div class="well">
-			<div class="row">
-				Documentos?
-			</div>
-			
-		</div>
-	</div>
-
- 	<div class="col-md-8">
+ 	<div class="col-md-10 col-md-offset-1">
  		<div class="well">
  		<div class="row">
- 			<div class="col-md-3">
+ 			<div class="col-md-4">
 	 			<div class="container">
-	 				<img id="profileImage" src="media/profileImages/test.jpg" alt="profile Image" style="height: 200px;width: 200px">
+	 				<img class="img-circle" id="profileImage" src="media/profileImages/test.jpg" alt="profile Image" style="height: 200px;width: 200px">
 	 			</div>
+ 				
+ 			</div>
+
+ 			<div class="col-md-8">
+ 				<div id="dataDump">
+ 					<div class="container">
+ 						<?php 
+ 							echo $user->getName();
+ 							echo $user->getSurname();
+ 							echo $user->getUser();
+ 							echo $user->getPhoto();
+ 						 ?>
+ 					</div>
+ 				</div>
  				
  			</div>
  		</div>
  		</div>
 
- 		<div class="well">
- 			<div class="row">
- 				<div id="data-dump" class="">
- 					<?php print_r($user);
 
 
+ 		<div class="row">
+ 			<div class="col-md-4">
+ 				<div class="well">
+					<div class="row">
+						<i class="fa fa-user fa-fw"></i>   <?= i18n("Friends") ?> 
 
- 					 ?>
- 				</div>
+
+					</div>
+					<div class="row">
+							<?php print_r($friends) ?>
+					</div>
+			
+				</div>
+				<div class="well">
+					<div class="row">
+						<i class="fa fa-user fa-fw"></i>   <?= i18n("Documents") ?> 
+
+
+					</div>
+					<div class="row">
+							<?php print_r($documents) ?>
+					</div>
+			
+				</div>
  			</div>
- 		</div>
- 		
+ 			<div class="col-md-8">
+
+				<!-- 1 por publicación -->
+				<div class="well">
+					<div class="row">
+						Publicación 1
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod incidunt rem quis perspiciatis dolore, maxime, ratione eum voluptas laudantium voluptatibus modi quam sequi culpa. Quia quae, eius incidunt tenetur magni.
+					</div>
+				</div>
+
+				<!-- 1 por publicación -->
+				<div class="well">
+					<div class="row">
+						Publicación 2
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde ipsam vel illo, doloribus repellat asperiores maiores aliquam quos a modi amet fugiat deserunt consequuntur, delectus cumque, repellendus laborum reiciendis hic.
+					</div>
+				</div>
+
+				<!-- 1 por publicación -->
+				<div class="well">
+					<div class="row">
+						<?php print_r($publications) ?>
+					</div>
+				</div>
+
+
+ 			</div>
+ 		</div>	
  		
  	</div>
+
  	
