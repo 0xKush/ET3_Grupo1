d4331c01dab00e7d0ac8db7e94262b3cb92a1170
Adrián Simón <adriansimn15@gmail.com>
2017-01-10 12:01:26 +0100

    usergroup/showall

--- Views/usergroup/USERGROUP_SHOWALL_Vista.php
+++ Views/usergroup/USERGROUP_SHOWALL_Vista.php
@@ -1,9 +1,12 @@
 <?php
 require_once(__DIR__."/../../core/ViewManager.php");
+require_once(__DIR__."/../../Models/USER_Model.php");
 $view = ViewManager::getInstance();
-$user = $view->getVariable("user");
+$currentuserid = $view->getVariable("currentuserid");
 $errors = $view->getVariable("errors");
 $groups = $view->getVariable("groups");
+
+$umapper = new USER_Model();
 ?>
 
 <?= isset($errors["general"])?$errors["general"]:"" ?> 
@@ -24,12 +27,52 @@ $groups = $view->getVariable("groups");
  				
  			</div>
  		</div>
-
- 		<div class="well">
- 			<div class="row">
- 				
- 			</div>
- 		</div>
+		
+		<?php foreach ($groups as $group): ?>
+				<div class="well">
+		 			<div class="row">
+		 				<?= $group->getName()?>
+		 			</div>
+		 			<div class="">
+		 				<a href="index.php?controller=user&action=showcurrent&id=<?= $group->getOwner()  ?>">
+		 				<?php $owner = $umapper->showcurrent($group->getOwner())?>
+		 				<?php if ($owner->getPhoto() != NULL): ?>
+		 					<img class="smallPhoto" src="media/profileImages/<?=$owner->getPhoto() ?>" alt="">
+		 				<?php else: ?>
+		 					<img class="smallPhoto" src="media/profileImages/default.png" alt="">
+		 				<?php endif ?>
+		 				<?= $owner->getUser(); ?></a>
+		 			</div>
+		 			<div class="row">
+		 				<?= $group->getDescription() ?>
+		 			</div>
+		 			<div class="row">
+		 			<?php if ($owner->getID() == $currentuserid): ?>
+		 				<div class="pull-right">
+			 				<a href="index.php?controller=group&action=delete&id=<?= $group->getID()  ?>">
+			 					<button class="btn btn-danger"><?= i18n("Delete") ?></button>
+			 				</a>
+			 			</div>
+			 		<?php else: ?>
+			 			<div class="pull-right">
+			 				<a href="index.php?controller=usergroup&action=delete&id=<?= $group->getID()  ?>">
+			 					<button class="btn btn-warning"><?= i18n("Unsubscribe") ?></button>
+			 				</a>
+			 			</div>
+		 			<?php endif ?>
+		 			
+		 				
+		 			
+		 				<div class="pull-right">
+			 				<a href="index.php?controller=group&action=showcurrent&id=<?= $group->getID()  ?>">
+			 					<button class="btn btn-primary"><?= i18n("View") ?></button>
+			 				</a>
+			 			</div>
+		 			</div>
+			 			
+		 		</div>
+		<?php endforeach ?>
+ 		
  		
  		
  	</div>
