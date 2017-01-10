8dfba98808e78b9a151d940e1d72060f55256a42
Cristopher <camartinez@esei.uvigo.es>
2017-01-10 16:34:50 +0100

    Fix

--- Models/FRIENDSHIP_Model.php
+++ Models/FRIENDSHIP_Model.php
@@ -1,83 +1,108 @@
 <?php
 
-require_once(__DIR__."/../core/PDOConnection.php");
+require_once(__DIR__ . "/../core/PDOConnection.php");
 
-class FRIENDSHIP_Model {
-       
+class FRIENDSHIP_Model
+{
     private $db;
-
+    
     public function __construct()
     {
         $this->db = PDOConnection::getInstance();
     }
-
-    public function showall($currentuserid, $add=1)
+    
+    public function showall($currentuserid, $add = 1)
     {
         $friends = array();
         
         $sql = $this->db->prepare("SELECT u.id, u.user, u.name, u.surname, u.photo FROM user as u, friendship as f where f.member=? AND f.status=? ORDER BY u.name");
-        $sql->execute(array($currentuserid, $add));
+        $sql->execute(array(
+            $currentuserid,
+            $add
+        ));
         $friends_db = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+        
         foreach ($friends_db as $friend) {
-            array_push($friends, new User($friend["id"], $friend["user"], $friend["name"], $friend["surname"]));
+            array_push($friends, new User($friend["id"], $friend["user"], $friend["name"], $friend["surname"]), NULL, NULL, NULL, NULL, NULL, $friend["photo"]);
         }
-
+        
         $sql = $this->db->prepare("SELECT u.id, u.user, u.name, u.surname, u.photo FROM user as u, friendship as f where f.secondarymember=? AND f.status=? ORDER BY u.name");
-        $sql->execute(array($currentuserid, $add));
+        $sql->execute(array(
+            $currentuserid,
+            $add
+        ));
         $friends_db = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+        
         foreach ($friends_db as $friend) {
-            array_push($friends, new User($friend["id"], $friend["user"], $friend["name"], $friend["surname"]));
+            array_push($friends, new User($friend["id"], $friend["user"], $friend["name"], $friend["surname"]), NULL, NULL, NULL, NULL, NULL, $friend["photo"]);
         }
-
+        
         return $friends;
     }
-
+    
     public function showcurrent($friendshipID)
     {
         $sql = $this->db->prepare("SELECT * FROM friendship WHERE id=?");
-        $sql->execute(array($friendshipID));
+        $sql->execute(array(
+            $friendshipID
+        ));
         $friendship = $sql->fetch(PDO::FETCH_ASSOC);
-
-        if($friendship != NULL) {
+        
+        if ($friendship != NULL) {
             return new Friendship($friendship["id"], $friendship["member"], $friendship["secondarymember"], $friendship["status"]);
         } else {
             return NULL;
         }
     }
-
-    public function add(Friendship $friendship) {
+    
+    public function add(Friendship $friendship)
+    {
         $sql = $this->db->prepare("INSERT INTO friendship(member,secondarymember,status) values (?,?,?)");
-        $sql->execute(array($friendship->getMember(), $friendship->getSecondaryMember(), $friendship->getStatus()));
+        $sql->execute(array(
+            $friendship->getMember(),
+            $friendship->getSecondaryMember(),
+            $friendship->getStatus()
+        ));
     }
-
+    
     public function edit(Friendship $friendship)
     {
         $sql = $this->db->prepare("UPDATE friendship SET member=?, secondarymember=?, status=? where id=?");
-        $sql->execute(array($friendship->getMember(), $friendship->getSecondaryMember(), $friendship->getStatus()));
+        $sql->execute(array(
+            $friendship->getMember(),
+            $friendship->getSecondaryMember(),
+            $friendship->getStatus()
+        ));
     }
-
+    
     public function delete(Friendship $friendship)
     {
         $sql = $this->db->prepare("DELETE FROM friendship where id=?");
-        $sql->execute(array($friendship->getID()));
+        $sql->execute(array(
+            $friendship->getID()
+        ));
     }
-
+    
     public function friendExists($member, $secondarymember)
     {
         $sql = $this->db->prepare("SELECT count(id) FROM friendship where member=? AND secondarymember=?");
-        $sql->execute(array($member, $secondarymember));
-
+        $sql->execute(array(
+            $member,
+            $secondarymember
+        ));
+        
         if ($sql->fetchColumn() > 0) {
             return true;
         }
-
+        
         $sql = $this->db->prepare("SELECT count(id) FROM friendship where member=? AND secondarymember=?");
-        $sql->execute(array($secondarymember, $member));
-
+        $sql->execute(array(
+            $secondarymember,
+            $member
+        ));
+        
         if ($sql->fetchColumn() > 0) {
             return true;
         }
-    }    
+    }
 }
