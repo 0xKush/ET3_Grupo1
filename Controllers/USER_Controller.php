df3df876e5989d1a7a33176f4f713f28f35576a0
Cristopher <camartinez@esei.uvigo.es>
2017-01-10 16:06:02 +0100

    User ->showcurrent updated
    Now, apart from the user, it also returns friends and publications

--- Controllers/USER_Controller.php
+++ Controllers/USER_Controller.php
@@ -1,36 +1,40 @@
 <?php
-require_once(__DIR__."/../core/ViewManager.php");
-require_once(__DIR__."/../core/I18n.php");
-require_once(__DIR__."/../core/Upload.php");
-require_once(__DIR__."/../Models/User.php");
-require_once(__DIR__."/../Models/USER_Model.php");
-require_once(__DIR__."/../Controllers/BaseController.php");
+require_once(__DIR__ . "/../core/ViewManager.php");
+require_once(__DIR__ . "/../core/I18n.php");
+require_once(__DIR__ . "/../core/Upload.php");
+require_once(__DIR__ . "/../Models/User.php");
+require_once(__DIR__ . "/../Models/USER_Model.php");
+require_once(__DIR__ . "/../Models/Publication.php");
+require_once(__DIR__ . "/../Models/PUBLICATION_Model.php");
+require_once(__DIR__ . "/../Models/Friendship.php");
+require_once(__DIR__ . "/../Models/FRIENDSHIP_Model.php");
+require_once(__DIR__ . "/../Controllers/BaseController.php");
 
-class USER_Controller extends BaseController {
-
+class USER_Controller extends BaseController
+{
     private $userModel;
-
+    
     public function __construct()
     {
         parent::__construct();
         $this->userModel = new USER_Model();
         $this->view->setLayout("base");
     }
-
+    
     public function login()
     {
-        if ($this->currentUser->getName()){
-            $this->view->redirect("publication", "showall","id=".$this->currentUser->getID());
+        if ($this->currentUser->getName()) {
+            $this->view->redirect("publication", "showall", "id=" . $this->currentUser->getID());
         }
         
-        if (isset($_POST["user"])){
+        if (isset($_POST["user"])) {
             if ($this->userModel->isValidUser($_POST["user"], $_POST["password"])) {
-                $user = $this->userModel->show_by_username($_POST["user"]);
-                $_SESSION["currentuser"] = $user->getUser();
+                $user                      = $this->userModel->show_by_username($_POST["user"]);
+                $_SESSION["currentuser"]   = $user->getUser();
                 $_SESSION["currentuserid"] = $user->getID();
-                $this->view->redirect("publication", "showall","id=".$this->currentUser->getID());
-            }else{
-                $errors = array();
+                $this->view->redirect("publication", "showall", "id=" . $this->currentUser->getID());
+            } else {
+                $errors            = array();
                 $errors["general"] = i18n("User is not valid");
                 $this->view->setVariable("errors", $errors);
             }
@@ -38,47 +42,62 @@ class USER_Controller extends BaseController {
         $this->view->setLayout("login");
         $this->view->render("user", "USER_LOGIN_Vista");
     }
-
-    public function home(){
-    	$this->view->redirect("publication", "showall","id=".$this->currentUser->getID());
+    
+    public function home()
+    {
+        $this->view->redirect("publication", "showall", "id=" . $this->currentUser->getID());
     }
-
+    
     public function showall()
     {
         $users = $this->userModel->showall();
         $this->view->setVariable("users", $users);
         $this->view->render("user", "USER_SHOWALL_Vista");
     }
-
+    
     public function showcurrent()
     {
         if (!isset($_REQUEST["id"])) {
             throw new Exception(i18n("An user id is mandatory"));
         }
-
+        
         $userid = $_REQUEST["id"];
-        $user = $this->userModel->showcurrent($userid);
-
+        $user   = $this->userModel->showcurrent($userid);
+        
         if ($user == NULL) {
-            throw new Exception(i18n("No such user with id: ").$userid);
+            throw new Exception(i18n("No such user with id: ") . $userid);
         }
-
+        
+        $publicationModel = new PUBLICATION_Model();
+        $publications     = $publicationModel->showall($userid, "user");
+        
+        
+        $friendshipModel = new FRIENDSHIP_Model();
+        $friends         = $this->friendshipModel->showall($userid);
+        
+        
+        
         $this->view->setVariable("user", $user);
+        
+        $this->view->setVariable("publications", $publications);
+        
+        $this->view->setVariable("friends", $friends);
+        
         $this->view->render("user", "USER_SHOWCURRENT_Vista");
     }
-
-
+    
+    
     public function add()
     {
-        $user = new User();
+        $user   = new User();
         $upload = new Upload();
         
         if (isset($_POST["submit"])) {
-            if (isset($_POST["file"])){
-                if ($upload->checkFile()){
+            if (isset($_POST["file"])) {
+                if ($upload->checkFile()) {
                     $user->setPhoto($upload->getDestination());
                 } else {
-                    $errors = array();
+                    $errors            = array();
                     $errors["general"] = i18n("Error while upload image");
                     $this->view->setVariable("errors", $errors);
                 }
@@ -92,37 +111,36 @@ class USER_Controller extends BaseController {
             $user->setBirthday($_POST["birthday"]);
             $user->setaddress($_POST["address"]);
             $user->setType($_POST["type"]);
-            if($_POST["status"] == "up"){
+            if ($_POST["status"] == "up") {
                 $user->setStatus(TRUE);
-            }
-            else{
+            } else {
                 $user->setStatus(FALSE);
             }
-            if($_POST["private"] == "private"){
+            if ($_POST["private"] == "private") {
                 $user->setPrivate(TRUE);
-            }
-            else{
+            } else {
                 $user->setPrivate(FALSE);
             }
             $user->setPassword($_POST["password"]);
             try {
-                if(!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])){
-                    if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])){
+                if (!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])) {
+                    if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
                         $user->checkIsValidForCreate();
                         $this->userModel->add($user);
-                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully added."),$user->getUser()));
+                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully added."), $user->getUser()));
                         $this->view->redirect("user", "login");
                     } else {
-                        $errors = array();
+                        $errors            = array();
                         $errors["general"] = i18n("email already exists");
                         $this->view->setVariable("errors", $errors);
                     }
                 } else {
-                    $errors = array();
+                    $errors            = array();
                     $errors["general"] = i18n("Username already exists");
                     $this->view->setVariable("errors", $errors);
                 }
-            }catch(ValidationException $ex) {
+            }
+            catch (ValidationException $ex) {
                 $errors = $ex->getErrors();
                 $this->view->setVariable("errors", $errors);
             }
@@ -131,28 +149,28 @@ class USER_Controller extends BaseController {
         $this->view->setVariable("user", $user);
         $this->view->render("user", "USER_ADD_Vista");
     }
-
+    
     public function edit()
     {
         $upload = new Upload();
-
+        
         if (!isset($_REQUEST["id"])) {
             throw new Exception(i18n("An user id is mandatory"));
         }
         
         $userid = $_REQUEST["id"];
-
+        
         $user = $this->userModel->showcurrent($userid);
-
+        
         if ($user == NULL) {
-            throw new Exception(i18n("No such user with id: ").$userid);
+            throw new Exception(i18n("No such user with id: ") . $userid);
         }
         
         if (isset($_POST["submit"])) {
-            if ($upload->checkFile()){
+            if ($upload->checkFile()) {
                 $user->setPhoto($upload->getDestination());
             } else {
-                $errors = array();
+                $errors            = array();
                 $errors["general"] = i18n("Error while upload image");
                 $this->view->setVariable("errors", $errors);
             }
@@ -163,69 +181,68 @@ class USER_Controller extends BaseController {
             $user->setBirthday($_POST["birthday"]);
             $user->setaddress($_POST["address"]);
             $user->setType($_POST["type"]);
-            if($_POST["status"] == "up"){
+            if ($_POST["status"] == "up") {
                 $user->setStatus(TRUE);
-            }
-            else{
+            } else {
                 $user->setStatus(FALSE);
             }
-            if($_POST["private"] == "private"){
+            if ($_POST["private"] == "private") {
                 $user->setPrivate(TRUE);
-            }
-            else{
+            } else {
                 $user->setPrivate(FALSE);
             }
             $user->setPassword($_POST["password"]);
-
+            
             try {
                 if ($user->getUser() == $_POST["user"] && $user->getEmail() == $_POST["email"]) {
                     $user->checkIsValidForCreate();
                     $this->userModel->edit($user);
-                    $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."),$user->getUser()));
+                    $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."), $user->getUser()));
                     $this->view->redirect("user", "login");
                 } else if ($user->getUser() == $_POST["user"] && $user->getEmail() != $_POST["email"]) {
-                    if(!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
+                    if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
                         $user->setEmail($_POST["email"]);
                         $user->checkIsValidForCreate();
                         $this->userModel->edit($user);
-                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."),$user->getUser()));
+                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."), $user->getUser()));
                         $this->view->redirect("user", "login");
                     } else {
-                        $errors = array();
+                        $errors            = array();
                         $errors["general"] = i18n("Email already exists or NULL");
                         $this->view->setVariable("errors", $errors);
                     }
                 } else if ($user->getUser() != $_POST["user"] && $user->getEmail() == $_POST["email"]) {
-                    if(!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])) {
+                    if (!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])) {
                         $user->setUser($_POST["user"]);
                         $user->checkIsValidForCreate();
                         $this->userModel->edit($user);
-                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."),$user->getUser()));
+                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."), $user->getUser()));
                         $this->view->redirect("user", "login");
                     } else {
-                        $errors = array();
+                        $errors            = array();
                         $errors["general"] = i18n("Username already exists or NULL");
                         $this->view->setVariable("errors", $errors);
                     }
-                } else if(!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
-                    if (!$this->userModel->userExists($_POST["user"] && !empty($_POST["user"]))){
+                } else if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
+                    if (!$this->userModel->userExists($_POST["user"] && !empty($_POST["user"]))) {
                         $user->setEmail($_POST["email"]);
                         $user->setUser($_POST["user"]);
                         $user->checkIsValidForCreate();
                         $this->userModel->edit($user);
-                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."),$user->getUser()));
+                        $this->view->setFlash(sprintf(i18n("User \"%s\" successfully updated."), $user->getUser()));
                         $this->view->redirect("user", "login");
                     } else {
-                        $errors = array();
+                        $errors            = array();
                         $errors["general"] = i18n("Username already exists");
                         $this->view->setVariable("errors", $errors);
                     }
-                } else{
-                    $errors = array();
+                } else {
+                    $errors            = array();
                     $errors["general"] = i18n("Email already exists or NULL");
                     $this->view->setVariable("errors", $errors);
                 }
-            }catch(ValidationException $ex) {
+            }
+            catch (ValidationException $ex) {
                 $errors = $ex->getErrors();
                 $this->view->setVariable("errors", $errors);
             }
@@ -242,17 +259,16 @@ class USER_Controller extends BaseController {
         }
         
         $userid = $_REQUEST["id"];
-        $user = $this->userModel->showcurrent($userid);
+        $user   = $this->userModel->showcurrent($userid);
         
         if ($user == NULL) {
-            throw new Exception(i18n("No such user with id: ").$userid);
+            throw new Exception(i18n("No such user with id: ") . $userid);
         }
         
         if (isset($_POST["submit"])) {
-            
-            if ($_POST["submit"] == "yes"){
+            if ($_POST["submit"] == "yes") {
                 $this->userMapper->delete($user);
-                $this->view->setFlash(sprintf(i18n("User \"%s\" successfully deleted."),$user->getUsername()));
+                $this->view->setFlash(sprintf(i18n("User \"%s\" successfully deleted."), $user->getUsername()));
             }
             
             $this->view->redirect("user", "showall");
@@ -261,100 +277,102 @@ class USER_Controller extends BaseController {
         $this->view->setVariable("user", $user);
         $this->view->render("user", "USER_DELETE_Vista");
     }
-
+    
     public function logout()
     {
         session_destroy();
         $this->view->redirect("user", "login");
     }
-
+    
     public function register()
     {
         $user = new User();
         
-        $user->setUser($_POST["user"]);;
+        $user->setUser($_POST["user"]);
         $user->setEmail($_POST["email"]);
         $user->setPassword($_POST["password"]);
-            
+        
         try {
-            if(!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])){
-                if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])){
+            if (!$this->userModel->userExists($_POST["user"]) && !empty($_POST["user"])) {
+                if (!$this->userModel->emailExists($_POST["email"]) && !empty($_POST["email"])) {
                     $user->checkIsValidForCreate();
                     $this->userModel->register($user);
                     $this->view->setFlash(sprintf(i18n("User \"%s\" successfully registered."), $user->getUser()));
                     $this->view->redirect("user", "login");
                 } else {
-                    $errors = array();
+                    $errors            = array();
                     $errors["general"] = i18n("email already exists");
                     $this->view->setVariable("errors", $errors);
                 }
             } else {
-                $errors = array();
+                $errors            = array();
                 $errors["general"] = i18n("Username already exists");
                 $this->view->setVariable("errors", $errors);
             }
-        }catch(ValidationException $ex) {
+        }
+        catch (ValidationException $ex) {
             $errors = $ex->getErrors();
             $this->view->setVariable("errors", $errors);
         }
-
+        
         $this->view->setVariable("user", $user);
         $this->view->redirect("user", "login");
     }
-
+    
     public function admin()
     {
         $this->view->render("user", "USER_ADMIN_Vista");
     }
-
-    public function search() {
+    
+    public function search()
+    {
         if (isset($_POST["submit"])) {
             $query = "";
-            $flag = 0;
-            if ($_POST["user"]){
-                $query .= "user LIKE '%". $_POST["user"] ."%'";
+            $flag  = 0;
+            if ($_POST["user"]) {
+                $query .= "user LIKE '%" . $_POST["user"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["name"]){
-                if ($flag){
+            if ($_POST["name"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "name LIKE '%". $_POST["name"] ."%'";
+                $query .= "name LIKE '%" . $_POST["name"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["surname"]){
-                if ($flag){
+            if ($_POST["surname"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "surname LIKE '%". $_POST["surname"] ."%'";
+                $query .= "surname LIKE '%" . $_POST["surname"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["email"]){
-                if ($flag){
+            if ($_POST["email"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "email LIKE '%". $_POST["email"] ."%'";
+                $query .= "email LIKE '%" . $_POST["email"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["type"]){
-                if ($flag){
+            if ($_POST["type"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "type LIKE '%". $_POST["type"] ."%'";
+                $query .= "type LIKE '%" . $_POST["type"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["status"]){
-                if ($flag){
+            if ($_POST["status"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "status LIKE '%". $_POST["status"] ."%'";
+                $query .= "status LIKE '%" . $_POST["status"] . "%'";
                 $flag = 1;
             }
-            if ($_POST["private"]){
-                if ($flag){
+            if ($_POST["private"]) {
+                if ($flag) {
                     $query .= " AND ";
                 }
-                $query .= "private LIKE '%". $_POST["private"] ."%'";
+                $query .= "private LIKE '%" . $_POST["private"] . "%'";
             }
             
             if (empty($query)) {
@@ -364,8 +382,7 @@ class USER_Controller extends BaseController {
             }
             $this->view->setVariable("users", $users);
             $this->view->render("user", "USER_SHOW_Vista");
-        }
-        else {
+        } else {
             $this->view->render("user", "USER_SEARCH_Vista");
         }
     }
