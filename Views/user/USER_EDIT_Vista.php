8aeb193f2fac40c1bf73f636f179a544fc0690d6
hacklego <igsalgado@esei.uvigo.es>
2017-01-11 01:09:16 +0100

    Fix user edit view

--- Views/user/USER_EDIT_Vista.php
+++ Views/user/USER_EDIT_Vista.php
@@ -12,96 +12,97 @@ $id = $_GET["id"];
 <?php print_r($errors) ?>
 
 
- <div class="container-fluid">
- 	
- 	<div class="col-md-8 col-md-offset-2" >
- 		<div class="well">
- 		<div class="container-fluid">
-	 		<div class="row">
-	 			<h1><?= i18n("Edit User: ")?><?= $user->getUser() ?></h1>
-	 		</div>
-	 		<div class="row">
-		 		<form>
-		 			<div class="form-group">
-					    <label for="name"><?= i18n("Name")?></label>
-					    <input type="text" class="form-control" id="name" name="name" placeholder="<?= $user->getName()?>">
-					  </div>
-					  <div class="form-group">
-					    <label for="surname"><?= i18n("Surname")?></label>
-					    <input type="text" class="form-control" id="surname" name="surname" placeholder="<?= $user->getSurname()?>">
-					  </div>
+<div class="container-fluid">
+    
+    <div class="col-md-8 col-md-offset-2" >
+ 	<div class="well">
+ 	    <div class="container-fluid">
+	 	<div class="row">
+	 	    <h1><?= i18n("Edit User: ")?><?= $user->getUser() ?></h1>
+	 	</div>
+	 	<div class="row">
+		    <form action="index.php?controller=user&action=edit" method="POST" enctype="multipart/form-data">
+		 	<div class="form-group">
+			    <label for="name"><?= i18n("Name")?></label>
+			    <input type="text" class="form-control" id="name" name="name" placeholder="<?= $user->getName()?>">
+			</div>
+			<div class="form-group">
+			    <label for="surname"><?= i18n("Surname")?></label>
+			    <input type="text" class="form-control" id="surname" name="surname" placeholder="<?= $user->getSurname()?>">
+			</div>
 
-					  <div class="form-group">
-					    <label for="password"><?= i18n("Password")?></label>
-					    <input type="password" class="form-control" id="password" name="password" placeholder="<?= $user->getPassword()?>">
-					  </div>
-					  <div class="form-group">
-					    <label for="phone"><?= i18n("Phone")?></label>
-					    <input type="number" class="form-control" id="phone" name="phone" placeholder="<?= $user->getPhone()?>">
-					  </div>
-					  <div class="form-group">
-					    <label for="address"><?= i18n("Adress")?></label>
-					    <input type="text" class="form-control" id="address" name="address" placeholder="<?= $user->getAddress()?>">
-					  </div>
-					  <div class="form-group">
-					    <label for="birthday"><?= i18n("Birth Date")?></label>
-					    <input type="text" class="form-control" id="birthday" name="birthday">
-					  </div>
-					  <div class="form-group">
-					    <label for="email"><?= i18n("Email")?></label>
-					    <input type="email" class="form-control" id="email" name="email" placeholder="<?= $user->getEmail() ?>">
-					   
+			<div class="form-group">
+			    <label for="password"><?= i18n("Password")?></label>
+			    <input type="password" class="form-control" id="password" name="password" placeholder="<?= $user->getPassword()?>">
+			</div>
+			<div class="form-group">
+			    <label for="phone"><?= i18n("Phone")?></label>
+			    <input type="number" class="form-control" id="phone" name="phone" placeholder="<?= $user->getPhone()?>">
+			</div>
+			<div class="form-group">
+			    <label for="address"><?= i18n("Adress")?></label>
+			    <input type="text" class="form-control" id="address" name="address" placeholder="<?= $user->getAddress()?>">
+			</div>
+			<div class="form-group">
+			    <label for="birthday"><?= i18n("Birth Date")?></label>
+			    <input type="text" class="form-control" id="birthday" name="birthday">
+			</div>
+			<div class="form-group">
+			    <label for="email"><?= i18n("Email")?></label>
+			    <input type="email" class="form-control" id="email" name="email" placeholder="<?= $user->getEmail() ?>">
+			</div>
+			
 
-					  <div class="form-group">
-					    <label for="type"><?= i18n("Privileges")?></label>
-					    <select class="form-control" id="type" name="type">
-					      <option value="0"><?= i18n("Base user")?></option>
-					      <option
-							<?php 
-							if ($user->getType()) {
-							 	echo " selected ";
-							 } ?>
-					       value="1"><?= i18n("Admin")?></option>
-					    </select>
-					  </div>
+			<div class="form-group">
+			    <label for="type"><?= i18n("Privileges")?></label>
+			    <select class="form-control" id="type" name="type">
+				<option value="0"><?= i18n("Base user")?></option>
+				<option
+				    <?php 
+				    if ($user->getType()) {
+					echo " selected ";
+				    } ?>
+value="1"><?= i18n("Admin")?></option>
+			    </select>
+			</div>
 
-					  <div class="form-group">
-					    <label for="private"><?= i18n("Visivility")?></label>
-					    <select class="form-control" id="private" name="private">
-					      <option value="public"><?= i18n("Public")?></option>
-					      <option
-							<?php 
-								if ($user->getPrivate()) {
-									echo " selected ";
-								}
-							 ?>
-					       value="private"><?= i18n("Private")?></option>
-					    </select>
-					  </div>
+			<div class="form-group">
+			    <label for="private"><?= i18n("Visivility")?></label>
+			    <select class="form-control" id="private" name="private">
+				<option value="public"><?= i18n("Public")?></option>
+				<option
+				    <?php 
+				    if ($user->getPrivate()) {
+					echo " selected ";
+				    }
+				    ?>
+value="private"><?= i18n("Private")?></option>
+			    </select>
+			</div>
 
-					  <div class="form-group">
-					    <label for="status"><?= i18n("Status")?></label>
-					    <select class="form-control" id="status" name="status">
-					      <option value="up"><?= i18n("Active")?></option>
-					      <option
-							<?php
-								if (!$user->getStatus()) {
-								 	echo " selected ";
-								 } 
-							 ?>
-					       value="down"><?= i18n("Down")?></option>
-					    </select>
-					  </div>
-					  <div class="form-group">
-					    <label for="file"><?= i18n("Profile Image")?></label>
-					    <input type="file" class="form-control-file" id="file" name="file">
-					    
-					  </div>
-					  <button type="submit" name="submit" class="btn btn-primary pull-right"><?= i18n("Submit")?></button>
-					</form>
-	 		</div>
- 		</div>
- 		</div>
- 	</div>	
- </div>
+			<div class="form-group">
+			    <label for="status"><?= i18n("Status")?></label>
+			    <select class="form-control" id="status" name="status">
+				<option value="up"><?= i18n("Active")?></option>
+				<option
+				    <?php
+				    if (!$user->getStatus()) {
+					echo " selected ";
+				    } 
+				    ?>
+value="down"><?= i18n("Down")?></option>
+			    </select>
+			</div>
+			<div class="form-group">
+			    <label for="file"><?= i18n("Profile Image")?></label>
+			    <input type="file" class="form-control-file" id="file" name="file">
+			    
+			</div>
+			<button type="submit" name="submit" class="btn btn-primary pull-right"><?= i18n("Submit")?></button>
+		    </form>
+	 	</div>
+ 	    </div>
+ 	</div>
+    </div>	
+</div>
 
