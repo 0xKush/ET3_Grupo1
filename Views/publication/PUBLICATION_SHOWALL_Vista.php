5223ce738ea876f7a09a73e04d182cbed68f03cb
Adrián Simón <adriansimn15@gmail.com>
2017-01-15 00:47:05 +0100

    basic showall 4 cristo

--- Views/publication/PUBLICATION_SHOWALL_Vista.php
+++ Views/publication/PUBLICATION_SHOWALL_Vista.php
@@ -19,7 +19,7 @@ $owners = $view->getVariable("owners");
 <?php $view->moveToDefaultFragment(); ?>
 
 <div class="container-fluid">
-	<div class="row"> 
+	<div class="row" style="margin-bottom: 10px"> 
 			<div class="pull-right">
 				<form action="index.php?controller=publication&action=add" method="post">
 					<input type="text" name="type" value="user" hidden="">
@@ -31,7 +31,36 @@ $owners = $view->getVariable("owners");
 	</div>
 
 	<div class="row">
-		
+		<?php foreach ($publications as $publication): ?>
+			<div class="panel">
+				<div class="panel-heading">
+					
+				</div>
+				<div class="panel-body">
+					<?= $publication->getDescription() ?>
+				</div>
+				<div class="panel-footer">
+				<div class="row">
+					
+				
+					<div class="col-xs-4 pull-left">
+						
+					</div>
+					<div class="col-xs-4">
+						
+					</div>
+					<div class="col-xs-4 pull-right">
+						<a href="index.php?controller=publication&action=showcurrent&id=<?=$publication->getID() ?>">
+							<button class="btn btn-primary">
+								<?= i18n("View") ?>
+								<i class="fa fa-eye"></i>
+							</button>
+						</a>
+					</div>
+					</div>
+				</div>
+			</div>
+		<?php endforeach ?>
 	</div>
 </div>
 
