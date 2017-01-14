8d7ca0b22ea26e50bb1c72d42eba139e92548f42
Cristopher <camartinez@esei.uvigo.es>
2017-01-14 13:32:44 +0100

    Event delete view fixed

--- Views/event/EVENT_DELETE_Vista.php
+++ Views/event/EVENT_DELETE_Vista.php
@@ -16,7 +16,7 @@
 
 	<div class="container">
 		<div class="col-xs-12 col-md-4 col-md-offset-4">
-		<form action="index.php?controller=group&event=delete&id=<?=$id ?>">
+		<form action="index.php?controller=event&action=delete&id=<?=$event->getID()?>" method="post">
 			<div class="well">
 				<div class="row">
 					<?= i18n("Delete Event: ") ?><?= $event->getName()?>
@@ -25,7 +25,7 @@
 					<div class="pull-right">
 					<a href="index.php?controller=event&action=showall">
 					<button type="button" class="btn btn-default"><?= i18n("No, go back") ?></button></a>                                                              
-                    <button type="submit" value="yes" class="btn btn-danger"><?= i18n("Yes, delete it ") ?></button>
+                    <button type="submit" name="submit" value="yes" class="btn btn-danger"><?= i18n("Yes, delete it ") ?></button>
 
 				</div>
 				</div>
