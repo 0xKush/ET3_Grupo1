2fd53322744979052d7db82d7dd39853c105d6ed
Adrián Simón <adriansimn15@gmail.com>
2017-01-15 00:26:11 +0100

    add publication

--- Views/publication/PUBLICATION_ADD_Vista.php
+++ Views/publication/PUBLICATION_ADD_Vista.php
@@ -1,10 +1,36 @@
+<?php
+require_once(__DIR__."/../../core/ViewManager.php");
+require_once(__DIR__."/../../Models/USER_Model.php");
+$view = ViewManager::getInstance();
+$currentuserid = $view->getVariable("currentuserid");
+$errors = $view->getVariable("errors");
 
+$type = $view->getVariable("type");
+
+$destiantion = $view->getVariable("destiantion");
+
+?>
 
 <div class="panel">
 	<div class="panel-heading">
-		<?= i18n("New Publication") ?>
+		<h1><?= i18n("New Publication") ?></h1>
 	</div>
-	<div class="panel-body">
-		
+	<div class="panel-body" style="padding: 20px">
+		<form action="index.php?controller=publication&action=add" method="post">
+
+		<div class="form-group">
+			<input id="imp" type="text" name="type" value="<?=$type ?>" hidden>
+			<input id="imp" type="text" name="destination" value="<?=$destination ?>" hidden>
+
+			<div class="row">
+					<label for="imp"><?= i18n("Content") ?>:</label>
+					<input class="form-control" rows="4" parsley-data-validate required id="imp" type="textarea" name="description">
+				<button class="btn btn-success" type="submit" name="submit" pull-right>
+				<?= i18n("Submit") ?>
+			</button>
+			</div>
+		</div>
+			
+		</form>
 	</div>
 </div>
--- Views/publication/PUBLICATION_SHOWALL_Vista.php
+++ Views/publication/PUBLICATION_SHOWALL_Vista.php
@@ -4,27 +4,34 @@ require_once(__DIR__."/../../Models/USER_Model.php");
 $view = ViewManager::getInstance();
 $currentuserid = $view->getVariable("currentuserid");
 $errors = $view->getVariable("errors");
+
 $publications = $view->getVariable("publications");
+
 $documents = $view->getVariable("documents");
 
-
-//publidoc -->array de k->v: [idpublicacion]->[iddocument]
 $publidoc = $view->getVariable("publidoc");
+
+$owners = $view->getVariable("owners");
 ?>
 
-<?php 
-	$umapper = new USER_Model();
- ?>
 
 <?= isset($errors["general"])?$errors["general"]:"" ?> 
 <?php $view->moveToDefaultFragment(); ?>
 
 <div class="container-fluid">
-	<div class="row">
-		<?php include(__DIR__.'/../../Views/publication/PUBLICATION_ADD_Vista.php'); ?>
+	<div class="row"> 
+			<div class="pull-right">
+				<form action="index.php?controller=publication&action=add" method="post">
+					<input type="text" name="type" value="user" hidden="">
+					<input type="text" name="destination" value="<?=$currentuserid ?>" hidden>
+
+					<button class="btn btn-success"><?= i18n("Create publication") ?></button>
+				</form>
+			</div>
 	</div>
 
 	<div class="row">
 		
 	</div>
 </div>
+
