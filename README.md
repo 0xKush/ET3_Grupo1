04f3723043fad1bbdf745a4145296794cc1713da
hacklego <igsalgado@esei.uvigo.es>
2017-01-15 04:51:24 +0100

    Instalation scripts + readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ET3_Grupo1
-Preparados para la ET4 shavale
+Rede Social Corporativa
+
+#Prerequisitos
+Ter a Máquina Virtual utilizada para o desenvolvemento, ofrecida polo profesor da asinatura
+
+#Instalacion
+* git clone https://github.com/0xKush/ET3_Grupo1.git
+* mv ET3Grupo1/* /var/www/html
+* Mediante o navegador accedede a ip_do_voso_servidor:porto/install.php (Solicitará usuario e password de root para mysql).
+* Os permisos deberían estar correctos pois estase a empregar git config core.fileMode true, en caso de existir problemas, dispomos dun script no raiz do proxecto chamado install.sh que configurará os permisos especiais necesarios.
+
+#Usuario Probas
+* nome: admin
+* contrasinal: admin
---
+++ install.php
@@ -0,0 +1,30 @@
+<form action="install.php" method="POST">
+    <p>Mysql Root User: <input type="text" name="user" /></p>
+    <p>Mysql Root Password:  <input type="password" name="passwd" /></p>
+    <p><input type="submit" name="submit" value="submit" /></p>
+</form>
+
+<?php
+if (isset($_POST["submit"])){
+
+    $mysqlUserName = $_POST["user"];
+    $mysqlPasswd = $_POST["passwd"];
+    $dbFile = "ET3Grupo1.sql";
+    $command='mysql -u' .$mysqlUserName .' -p' .$mysqlPasswd . ' < ' .$dbFile;
+    exec($command,$output=array(),$worked);
+    switch($worked){
+	case 0:
+            echo 'Import file <b>' .$dbFile .'</b> successfully imported to database<br />';
+            echo 'Database user created: ET3Grupo1 with password ET3Grupo1<br />';
+            echo 'Test users created: admin with password admin, also name with password name for other users<br />';
+            echo '<a href="index.php">Proceed to Website</a><br />';
+            break;
+	case 1:
+            echo 'There was an error during import: User already Exists or You insert bad credentials<br /><br />';
+            echo 'Database user created: moovett with password moovett<br /><br />';
+            echo 'Website user admin created: admin with password admin<br /><br />';
+            echo '<a href="index.php">Check if credentials are ok and Proceed to Website</a>';
+            break;
+    }
+}
+?>
